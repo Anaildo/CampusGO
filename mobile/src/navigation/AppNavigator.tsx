0530e47e87c4a34c58f34bd66f0f36d662092ee1
Anaildo silva <anaildonascimentosilva074@gmail.com>
2025-12-26 23:34:24 -0300

    ✨feat: add navegação login

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -2,20 +2,30 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SplashScreen from '../screens/onboarding/SplashScreen';
-import WelcomeScreen from '../screens/onboarding/WelcomeScreen';
+import { WelcomeScreen } from '../screens/onboarding/WelcomeScreen';
+import { LoginScreen } from '../screens/onboarding/auth/login';
 
-type RootStackParamList = {
+
+export type RootStackParamList = {
   Splash: undefined;
   Welcome: undefined;
+  Login: undefined;
+  Register: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default function AppNavigator(): React.ReactElement {
+export default function AppNavigator() {
   return (
-    <Stack.Navigator id="Root" initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      id="root"
+      initialRouteName="Splash"
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      
     </Stack.Navigator>
   );
 }
