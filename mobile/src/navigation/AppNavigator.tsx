5130c2d0e5e5741f267b1ad9178fb7c14b52a7a4
Anaildo silva <anaildonascimentosilva074@gmail.com>
2025-12-26 23:47:24 -0300

    ✨ feat: add navegação cadastro

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from '../screens/onboarding/SplashScreen';
 import { WelcomeScreen } from '../screens/onboarding/WelcomeScreen';
 import { LoginScreen } from '../screens/onboarding/auth/login';
-
+import { RegisterScreen } from '../screens/onboarding/auth/register'; 
 
 export type RootStackParamList = {
   Splash: undefined;
@@ -25,7 +25,7 @@ export default function AppNavigator() {
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
-      
+       <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 }
