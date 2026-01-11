28086bccd1606c52bc57acb06afa0f812bf38748
Camile <camileisi@alu.ufc.br>
2026-01-11 14:50:42 -0300

    :sparkles:feat(backend):adiciona o backend da aplicação

---
+++ backend/src/data/onibus.ts
@@ -0,0 +1,19 @@
+export type Onibus = {
+  id: number;
+  nome: string;
+};
+
+export const onibusMock: Onibus[] = [
+  {
+    id: 1,
+    nome: 'Ônibus da Prefeitura (PMQ)',
+  },
+  {
+    id: 2,
+    nome: 'Ônibus da UFC',
+  },
+  {
+    id: 3,
+    nome: 'Ônibus Noturno',
+  },
+];
---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import { onibusMock } from "./data/onibus";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/onibus", (req, res) => {
+  res.json(onibusMock);
+});
+
+const PORT = Number(process.env.PORT) || 3333;
+
+app.listen(PORT, () => {
+  console.log(`🚍 Backend rodando na porta ${PORT}`);
+});
